e5eea50393b034c1ab8b8c971952a0ed99eb5575
Pastilhas <joao.campos1221@gmail.com>
2019-05-06 15:56:58 +0100

    Update server.c

---
+++ server.c
@@ -0,0 +1,17 @@
+#include "type.h"
+#include "sope.h"
+
+// Server Program
+
+int main(int argc, char *argv[]) {
+    //server <BOX OFFICES> <NAME>
+
+    //OPEN FIFO TO READ
+    //CRIAR CONTA ADMIN
+    //CRIAR BOX OFFICES
+
+
+    //FECHAR FIFO
+
+    return 0;
+}
