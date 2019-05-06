7a909ab4c0cee0fdb6951811b9351f7020f831ca
Pastilhas <joao.campos1221@gmail.com>
2019-05-06 16:23:47 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,10 +1,36 @@
 #include "type.h"
 #include "sope.h"
+#include "types.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sope.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 // Server Program
 
 int main(int argc, char *argv[]) {
     //server <BOX OFFICES> <NAME>
+    int number_threads = strtol(argv[1]);
+    if(number_threads <= 0 || number_threads > MAX_BANK_OFFICES)    exit(1);
+
+
+    sem_t* sem = sem_open(argv[2], O_CREAT, 0600, number_threads);
+    if(sem == SEM_FAILED)   exit(2);
+
+    pthread_mutex_t *mutex_array[number_threads];
+
+    for(int i = 0; i < number_threads; i++){
+        //criar mutex
+        pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+        //criar thread
+    }
+
+
 
     //OPEN FIFO TO READ
     //CRIAR CONTA ADMIN
