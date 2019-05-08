6c2741bf20b19d91de022a80b9b36242176ff9f4
Pastilhas <joao.campos1221@gmail.com>
2019-05-08 19:52:31 +0100

    .

--- box_office.h
+++ box_office.h
@@ -1,6 +1,23 @@
+#include "types.h"
+#include "sope.h"
+#include "types.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sope.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+
 struct thread_arg{
     sem_t *sem;
     pthread_mutex_t mutex;
 };
 
 void* box_office(void* arg);
+
+int log_in();
+int get_operation();
+int do_stuff();
