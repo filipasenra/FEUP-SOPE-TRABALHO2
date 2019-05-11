360d0abe84cbd7c52894dddf7ca4510358aabfc8
Pastilhas <joao.campos1221@gmail.com>
2019-05-11 14:16:53 +0100

    Desisti de fazer o request e reply numa thread
    Ao ler informação online nao vale a pena o trabalho

--- communication.c
+++ communication.c
@@ -2,116 +2,81 @@
 #include "serverMessage.h"
 
 int sendReply(tlv_request_t *user_request, tlv_reply_t *user_reply) {
-    //Create FIFO to receive request
+    // Create FIFO to receive request
     int fdr;
     mkfifo(SERVER_FIFO_PATH, 0666);
 
-    //Open FIFO to receive request
-    if ((fdr = open(SERVER_FIFO_PATH, O_RDONLY)) < 0)
-        return RC_OTHER;
+    // Open FIFO to receive request
+    if ((fdr = open(SERVER_FIFO_PATH, O_RDONLY)) < 0) return RC_OTHER;
 
-    //Receiving request
+    // Receiving request
     read(fdr, user_request, sizeof(user_reply));
 
-    //Make operation requested
-    //TODO
+    // Make operation requested
+    // TODO
 
+    // Preparing reply
+    if (replyMessageTLV(user_request, user_reply)) return RC_USR_DOWN;
 
-
-
-    
-
-    //Preparing reply
-    if (replyMessageTLV(user_request, user_reply))
-        return RC_USR_DOWN;
-
-    //Open FIFO to send reply
+    // Open FIFO to send reply
     int fda;
-    char *fifo_send = malloc(sizeof(USER_FIFO_PATH_PREFIX) + sizeof(user_request->value.header.pid));
+    char *fifo_send = malloc(sizeof(USER_FIFO_PATH_PREFIX) +
+                             sizeof(user_request->value.header.pid));
     if ((fda = open(user_request->value.header.pid, O_WRONLY)) < 0)
         return RC_OTHER;
 
-    //Sending reply
+    // Sending reply
     write(fda, user_reply, sizeof(user_reply));
 
-    //Free memory
+    // Free memory
     free(fifo_send);
 
     return RC_OK;
 }
 
-int setCommunication(tlv_request_t *user_request, tlv_reply_t *user_reply) {
-    //Create FIFO to receive reply
-    char *fifo_receive = malloc(sizeof(USER_FIFO_PATH_PREFIX) + sizeof(getpid()));
-    sprintf(fifo_receive, "%s%d", USER_FIFO_PATH_PREFIX, getpid());
-    mkfifo(fifo_receive, 0666);
-
-    //Open FIFO to send request
-    int fdr;
-    if ((fdr = open(SERVER_FIFO_PATH, O_WRONLY)) < 0)
-        return RC_OTHER;
-
-    //Sending request
-    write(fdr, user_request, sizeof(user_request));
-
-    //Open FIFO to receive answer
+int get_reply(tlv_reply_t reply) {
     int fda;
-    if ((fda = open(fifo_receive, O_RDONLY)) < 0)
-        return RC_OTHER;
-    
-    //Receiving reply
     clock_t initial = clock();
-    while(read(fda, user_reply, sizeof(user_reply))) {
-        if (((clock()-initial)/CLOCKS_PER_SEC) == FIFO_TIMEOUT_SECS) {
+
+    // Create FIFO
+    char *fifo_reply = malloc(sizeof(USER_FIFO_PATH_PREFIX) + sizeof(getpid()));
+    sprintf(fifo_reply, "%s%d", USER_FIFO_PATH_PREFIX, getpid());
+    if (mkfifo(fifo_reply, 0666)) return NULL;
+
+    // Open FIFO
+    if ((fda = open(fifo_reply, O_RDONLY)) < 0) return RC_OTHER;
+
+    // Receiving reply
+    while (read(fda, &reply, sizeof(reply))) {
+        if (((clock() - initial) / CLOCKS_PER_SEC) == FIFO_TIMEOUT_SECS) {
             printf("Action took too long...\n");
             close(fda);
-            close(fdr);
             return RC_SRV_TIMEOUT;
         }
     }
 
-    //Closing FIFOS
-    if (close(fdr) || close(fda))
-        return RC_OTHER;
+    // Closing FIFOS
+    if (close(fda)) return RC_OTHER;
 
-    //Free memory
-    unlink(fifo_receive);
-    free(fifo_receive);
+    // Free memory
+    unlink(fifo_reply);
+    free(fifo_reply);
 
     return RC_OK;
 }
 
-/*void* get_reply(void* arg){
-    tlv_reply_t* reply = (tlv_reply_t*)arg;
-
-    //Create FIFO
-    char *fifo_reply = malloc(sizeof(USER_FIFO_PATH_PREFIX) + sizeof(getpid()));
-    sprintf(fifo_reply, "%s%d", USER_FIFO_PATH_PREFIX, getpid());
-    if(mkfifo(fifo_reply, 0666))    return NULL;
-
-    //Open FIFO
-    int fda;
-    if ((fda = open(fifo_reply, O_RDONLY)) < 0)
-        return RC_OTHER;
-    
-    //Receiving reply
+int send_request(tlv_request_t request) {
+    int fdr;
     clock_t initial = clock();
-    while(read(fda, &user_reply, sizeof(user_reply))) {
-        if (((clock()-initial)/CLOCKS_PER_SEC) == FIFO_TIMEOUT_SECS) {
-            printf("Action took too long...\n");
-            close(fda);
-            close(fdr);
-            return RC_SRV_TIMEOUT;
-        }
-    }
 
-    //Closing FIFOS
-    if (close(fdr) || close(fda))
-        return RC_OTHER;
+    // Open FIFO
+    if ((fdr = open(SERVER_FIFO_PATH, O_WRONLY)) < 0) return RC_OTHER;
 
-    //Free memory
-    unlink(fifo_reply);
-    free(fifo_reply);
+    // Send request
+    write(fdr, user_request, sizeof(user_request));
 
-    return reply;
-}*/
+    // Closing FIFOS
+    if (close(fdr)) return RC_OTHER;
+
+    return RC_OK;
+}
