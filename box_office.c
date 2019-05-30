53bcff8155878849368b046293a854cbf914015a
Patilhas <joao.campos1221@gmail.com>
2019-05-30 23:33:44 +0100

    Revert "changed locks"
    This reverts commit 634c5368b06cc66c50ffd27fcfd20c91f8c4dc92.

--- box_office.c
+++ box_office.c
@@ -2,37 +2,36 @@
 
 int n_array = -1;
 
-void *box_office(void *arg) {
+void *box_office(void *arg)
+{
     tlv_request_t request;
     tlv_reply_t reply;
 
-    for (int n = 0; n < number_threads; n++) {
-        if (thread_array[n] == pthread_self()) {
+    for (int n = 0; n < number_threads; n++)
+    {
+        if (thread_array[n] == pthread_self())
+        {
             n_array = n + 1;
             break;
         }
     }
 
-    while (1) {
+    while (1)
+    {
         sem_wait(&n_req);
         int value = -1;
         sem_getvalue(&b_off, &value);
-        logSyncMechSem(*(int *)arg, n_array, SYNC_OP_SEM_WAIT,
-                       SYNC_ROLE_CONSUMER, request.value.header.account_id,
-                       value);
+        logSyncMechSem(*(int *)arg, n_array, SYNC_OP_SEM_WAIT, SYNC_ROLE_CONSUMER, request.value.header.account_id, value);
 
         pthread_mutex_lock(&q_mutex);
-        logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_LOCK,
-                    SYNC_ROLE_CONSUMER, request.value.header.account_id);
+        logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_CONSUMER, request.value.header.account_id);
 
-        request = front(queue);  // Gets the request that arrived first
+        request = front(queue); // Gets the request that arrived first
 
-        pop(&queue);  // Updates the queue and 'frees' the space ocupided by the
-                      // request picked up by this thread
+        pop(&queue); // Updates the queue and 'frees' the space ocupided by the request picked up by this thread
 
         pthread_mutex_unlock(&q_mutex);
-        logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_UNLOCK,
-                    SYNC_ROLE_CONSUMER, request.value.header.account_id);
+        logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_CONSUMER, request.value.header.account_id);
 
         // Handles the request
         reply.value.header.account_id = request.value.header.account_id;
@@ -40,52 +39,88 @@ void *box_office(void *arg) {
         reply.length = sizeof(rep_header_t);
         reply.type = request.type;
 
+        logSyncDelay(*(int *)arg, n_array, request.value.header.account_id, request.value.header.op_delay_ms * 1000);
+        usleep(request.value.header.op_delay_ms * 1000);
+
         int index;
 
-        if ((index = log_in(&db, request.value.header.account_id, request.value.header.password)) != -1) {
-            logSyncDelay(*(int *)arg, n_array, request.value.header.account_id, request.value.header.op_delay_ms * 1000);
-            usleep(request.value.header.op_delay_ms * 1000);
+        if ((index = log_in(&db, request.value.header.account_id, request.value.header.password)) != -1)
+        {
+
+            pthread_mutex_lock(&db_mutex[index]);
+            logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_ACCOUNT, request.value.header.account_id);
 
             int op = (int)request.type;
             bank_account_t acc;
 
-            switch (op) {
-                case 0:  // CREATE
-                    if (request.value.header.account_id != 0) { reply.value.header.ret_code = RC_OP_NALLOW; break; }
+            switch (op)
+            {
+            case 0: // CREATE
+                if (request.value.header.account_id != 0)
+                {
+                    reply.value.header.ret_code = RC_OP_NALLOW;
+                    break;
+                }
+                if (get_account(request.value.create.account_id, &db) != -1)
+                {
+                    reply.value.header.ret_code = RC_ID_IN_USE;
+                    break;
+                }
 
-                    int tmp_index;
-                    if ((tmp_index = get_account(request.value.create.account_id, &db, *(int *)arg, n_array, request.value.header.account_id)) != -1) {
-                        reply.value.header.ret_code = RC_ID_IN_USE;
-                        unlock_account(tmp_index); break;
-                    }
+                create_account(&acc, request.value.create.password, request.value.create.account_id, request.value.create.balance, &reply, *(int *)arg);
 
-                    create_account(&acc, request.value.create.password, request.value.create.account_id, request.value.create.balance, &reply, *(int *)arg);
+                if (add_account(acc, &db))
+                {
+                    reply.value.header.ret_code = RC_OTHER;
+                    break;
+                }
 
-                    if (add_account(acc, &db, *(int *)arg, n_array, request.value.header.account_id))) { reply.value.header.ret_code = RC_OTHER; break; }
+                break;
+            case 1: // CHECK BALANCE
+                if (request.value.header.account_id == 0)
+                {
+                    reply.value.header.ret_code = RC_OP_NALLOW;
+                    break;
+                }
 
+                acc = db.dataBaseArray[index];
+                check_balance(&acc, &reply);
+                break;
+            case 2: // TRANSFER
+                if (request.value.header.account_id == 0)
+                {
+                    reply.value.header.ret_code = RC_OP_NALLOW;
                     break;
-                case 1:  // CHECK BALANCE
-                    if (request.value.header.account_id == 0) { reply.value.header.ret_code = RC_OP_NALLOW; break; }
-                    acc = db.dataBaseArray[index];
-                    check_balance(&acc, &reply);
+                }
+
+                transfer(request, &reply, *(int *)arg, request.value.header.op_delay_ms * 1000);
+
+                break;
+            case 3: // SHUTDOWN
+                if (request.value.header.account_id != 0)
+                {
+                    reply.value.header.ret_code = RC_OP_NALLOW;
                     break;
-                case 2:  // TRANSFER
-                    if (request.value.header.account_id == 0) { reply.value.header.ret_code = RC_OP_NALLOW; break; }
-                    transfer(index, request, &reply, *(int *)arg, request.value.header.op_delay_ms * 1000);
-                    break;
-                case 3:  // SHUTDOWN
-                    if (request.value.header.account_id != 0) { reply.value.header.ret_code = RC_OP_NALLOW; break; }
-                    shutdown(&reply);
-                    break;
-                default:
-                    break;
+                }
+                shutdown(&reply);
+                break;
+            default:
+                break;
             }
-            unlock_account(index);
-        } else {
+
+            pthread_mutex_unlock(&db_mutex[index]);
+            logSyncMech(*(int *)arg, n_array, SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_ACCOUNT, request.value.header.account_id);
+        }
+        else
+        {
             reply.value.header.ret_code = RC_LOGIN_FAIL;
         }
 
-        if (send_reply(request.value.header.pid, &reply) != RC_OK) { reply.value.header.ret_code = RC_USR_DOWN; logReply(STDOUT_FILENO, n_array, &reply); }
+        if (send_reply(request.value.header.pid, &reply) != RC_OK)
+        {
+            reply.value.header.ret_code = RC_USR_DOWN;
+            logReply(STDOUT_FILENO, n_array, &reply);
+        }
 
         logReply(*(int *)arg, n_array, &reply);
 
@@ -97,7 +132,9 @@ void *box_office(void *arg) {
     return NULL;
 }
 
-int create_account(bank_account_t *account, char password[], int accound_id, int balance, tlv_reply_t *user_reply, int fd) {
+int create_account(bank_account_t *account, char password[], int accound_id,
+                   int balance, tlv_reply_t *user_reply, int fd)
+{
     account->account_id = accound_id;
     account->balance = balance;
 
@@ -112,10 +149,15 @@ int create_account(bank_account_t *account, char password[], int accound_id, int
     return 0;
 }
 
-int check_balance(bank_account_t *bank_account, tlv_reply_t *user_reply) {
+int check_balance(bank_account_t *bank_account, tlv_reply_t *user_reply)
+{
     user_reply->type = OP_BALANCE;
 
-    if (bank_account == NULL) { user_reply->value.header.ret_code = RC_ID_NOT_FOUND; return RC_ID_NOT_FOUND; }
+    if (bank_account == NULL)
+    {
+        user_reply->value.header.ret_code = RC_ID_NOT_FOUND;
+        return RC_ID_NOT_FOUND;
+    }
 
     user_reply->value.balance.balance = bank_account->balance;
     user_reply->length += sizeof(rep_balance_t);
@@ -124,18 +166,27 @@ int check_balance(bank_account_t *bank_account, tlv_reply_t *user_reply) {
     return 0;
 }
 
-int transfer(int index_header, tlv_request_t user_request, tlv_reply_t *user_reply, int fd, int delay) {
+int transfer(tlv_request_t user_request, tlv_reply_t *user_reply, int fd, int delay)
+{
     user_reply->type = OP_TRANSFER;
-
-    int index = get_account(user_request.value.transfer.account_id, &db, *(int *)arg, n_array, request.value.header.account_id);
-
-    if (index == -1) { user_reply->value.header.ret_code = RC_ID_NOT_FOUND; return RC_ID_NOT_FOUND; }
 
     logSyncDelay(fd, n_array, user_request.value.transfer.account_id, delay);
     usleep(delay);
 
+    int index = get_account(user_request.value.transfer.account_id, &db);
+
+    if (index == -1)
+    {
+        user_reply->value.header.ret_code = RC_ID_NOT_FOUND;
+        return RC_ID_NOT_FOUND;
+    }
+
+    pthread_mutex_lock(&(db_mutex[index]));
+    logSyncMech(fd, n_array, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_ACCOUNT, user_request.value.transfer.account_id);
+
     bank_account_t *bank_acc_dest = &(db.dataBaseArray[index]);
 
+    int index_header = get_account(user_request.value.header.account_id, &db);
     bank_account_t *bank_acc_orig = &(db.dataBaseArray[index_header]);
 
     int amount = user_request.value.transfer.amount;
@@ -143,14 +194,17 @@ int transfer(int index_header, tlv_request_t user_request, tlv_reply_t *user_rep
     user_reply->value.transfer.balance = bank_acc_orig->balance;
     user_reply->length += sizeof(rep_transfer_t);
 
-    if ((bank_acc_orig->balance - amount) < MIN_BALANCE) {
-        unlock_account(index);
+    if ((bank_acc_orig->balance - amount) < MIN_BALANCE)
+    {
+        pthread_mutex_unlock(&db_mutex[index]);
         user_reply->value.header.ret_code = RC_NO_FUNDS;
         return RC_NO_FUNDS;
     }
 
-    if ((bank_acc_dest->balance + amount) > MAX_BALANCE) {
-        unlock_account(index);
+    if ((bank_acc_dest->balance + amount) > MAX_BALANCE)
+    {
+
+        pthread_mutex_unlock(&db_mutex[index]);
         user_reply->value.header.ret_code = RC_TOO_HIGH;
         return RC_TOO_HIGH;
     }
@@ -158,7 +212,8 @@ int transfer(int index_header, tlv_request_t user_request, tlv_reply_t *user_rep
     bank_acc_orig->balance -= amount;
     bank_acc_dest->balance += amount;
 
-    unlock_account(index, fd, n_array, request.value.header.account_id));
+    pthread_mutex_unlock(&(db_mutex[index]));
+    logSyncMech(fd, n_array, SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_ACCOUNT, user_request.value.transfer.account_id);
 
     user_reply->value.transfer.balance = bank_acc_orig->balance;
     user_reply->value.header.ret_code = RC_OK;
@@ -166,7 +221,8 @@ int transfer(int index_header, tlv_request_t user_request, tlv_reply_t *user_rep
     return 0;
 }
 
-void shutdown(tlv_reply_t *user_reply) {
+void shutdown(tlv_reply_t *user_reply)
+{
     int value = 1;
     sem_getvalue(&b_off, &value);
 
--- dataBase.h
+++ dataBase.h
@@ -5,8 +5,6 @@
 #include <string.h>
 #include "creatAccount.h"
 
-extern pthread_mutex_t db_mutex[MAX_BANK_ACCOUNTS];
-
 /**
  * Represents a dataBase
 */
@@ -26,20 +24,14 @@ void init_database(dataBase_t *dataBase);
  * 
  * @return Returns zero upon sucess, non-zero otherwise
 */
-int add_account(bank_account_t bank_account, dataBase_t *dataBase, int fd, int n_array, tlv_request_t user_request);
+int add_account(bank_account_t bank_account, dataBase_t *dataBase);
 
 /**
  * @brief Gets the index of the account with the account_id given
  * 
  * @return Returns index upon sucess, -1 otherwise
 */
-int get_account(int account_id, dataBase_t *dataBase, int fd, int n_array, tlv_request_t user_request);
-
-/**
- * @brief Unlocks mutex of given account
- * @return Returns index
-*/
-int unlock_account(int index, int fd, int n_array, tlv_request_t user_request){
+int get_account(int account_id, dataBase_t *dataBase);
 
 
 /**
