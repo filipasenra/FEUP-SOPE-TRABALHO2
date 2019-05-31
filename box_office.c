e78f1e262786ff2f4252917eff3391d390401e21
Patilhas <joao.campos1221@gmail.com>
2019-05-31 09:29:04 +0100

    debug at box_office.c

--- box_office.c
+++ box_office.c
@@ -53,7 +53,7 @@ void *box_office(void *arg) {
                                                                                                                                                             write(STDERR_FILENO, "CREATE\n", 7);
                     if (request.value.header.account_id != 0) { reply.value.header.ret_code = RC_OP_NALLOW; break; }
                     if (get_account(request.value.create.account_id, &db) != -1) { reply.value.header.ret_code = RC_ID_IN_USE; break; }
-                    new_index = db->last_element;
+                    new_index = db.last_element;
                     lock_account(new_index, fd_log, request);
                     create_account(&acc, request.value.create.password, request.value.create.account_id, request.value.create.balance, &reply, fd_log);
                     if (add_account(acc, &db)) { reply.value.header.ret_code = RC_OTHER; break; }
@@ -73,6 +73,7 @@ void *box_office(void *arg) {
                     shutdown(&reply);
                     break;
             }
+                                                                                                                                                            write(STDERR_FILENO, "FINISH\n", 7);
 
             unlock_account(index, fd_log, request);
         } else { reply.value.header.ret_code = RC_LOGIN_FAIL; }
