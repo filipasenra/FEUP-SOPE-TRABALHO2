a8d4fe837505c480adb56ea954a682cb102588d9
Pastilhas <joao.campos1221@gmail.com>
2019-05-31 22:10:13 +0100

    removed prints

--- user.c
+++ user.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
     // CREATE FIFO REPLY
     char *fifo_reply = malloc(sizeof(USER_FIFO_PATH_PREFIX) + sizeof(pid_t));
     sprintf(fifo_reply, "%s%d", USER_FIFO_PATH_PREFIX, getpid());
-    printf("%s\n", fifo_reply);
     unlink(fifo_reply);
     if (mkfifo(fifo_reply, 0666))
         return RC_OTHER;
--- userMessage.c
+++ userMessage.c
@@ -1,7 +1,6 @@
 #include "userMessage.h"
 
-int prepareMainArgs(char *argv[], tlv_request_t *user_request)
-{
+int prepareMainArgs(char *argv[], tlv_request_t *user_request) {
     // Preparing values
     uint32_t account_id;
     uint32_t delay;
@@ -10,14 +9,11 @@ int prepareMainArgs(char *argv[], tlv_request_t *user_request)
 
     delay = strtoul(argv[3], NULL, 10);
 
-    //Verify passord
-    if (strlen(argv[2]) > MAX_PASSWORD_LEN + 1)
-    {
+    // Verify passord
+    if (strlen(argv[2]) > MAX_PASSWORD_LEN + 1) {
         printf("Password too long\n");
         return RC_OTHER;
-    }
-    else if (strlen(argv[2]) < MIN_PASSWORD_LEN)
-    {
+    } else if (strlen(argv[2]) < MIN_PASSWORD_LEN) {
         printf("Password too short\n");
         return RC_OTHER;
     }
@@ -32,89 +28,79 @@ int prepareMainArgs(char *argv[], tlv_request_t *user_request)
     return RC_OK;
 }
 
-int prepareTypeOfOpArgs(char *argv[], tlv_request_t *user_request)
-{
+int prepareTypeOfOpArgs(char *argv[], tlv_request_t *user_request) {
     // Preparing values
     uint32_t account_id = user_request->value.header.account_id;
 
     // TYPE OF OPERATION
     int n_type = strtoul(argv[4], NULL, 10);
 
-    switch (n_type)
-    {
-    case 0:
-        user_request->type = OP_CREATE_ACCOUNT;
+    switch (n_type) {
+        case 0:
+            user_request->type = OP_CREATE_ACCOUNT;
 
-        if (createAccountUser(&user_request->value.create, argv[5]) != 0)
+            if (createAccountUser(&user_request->value.create, argv[5]) != 0)
+                return RC_OTHER;
+
+            user_request->length += sizeof(req_create_account_t);
+
+            break;
+        case 1:
+            user_request->type = OP_BALANCE;
+            break;
+
+        case 2:
+            user_request->type = OP_TRANSFER;
+
+            int n;
+            if ((n = transferOperation(
+                     account_id, &user_request->value.transfer, argv[5])) != 0)
+                return n;
+
+            user_request->length += sizeof(req_transfer_t);
+
+            break;
+        case 3:
+
+            user_request->type = OP_SHUTDOWN;
+
+            break;
+        default:
+
+            printf("Invalid Type\n");
             return RC_OTHER;
-
-        user_request->length += sizeof(req_create_account_t);
-
-        break;
-    case 1:
-        user_request->type = OP_BALANCE;
-        break;
-
-    case 2:
-        user_request->type = OP_TRANSFER;
-
-        int n;
-        if ((n = transferOperation(
-                 account_id, &user_request->value.transfer, argv[5])) != 0)
-            return n;
-
-        user_request->length += sizeof(req_transfer_t);
-
-        break;
-    case 3:
-
-        user_request->type = OP_SHUTDOWN;
-        
-        break;
-    default:
-
-        printf("Invalid Type\n");
-        return RC_OTHER;
     }
 
     return RC_OK;
 }
 
-int requestMessageTLV(int argc, char *argv[], tlv_request_t *user_request)
-{
+int requestMessageTLV(int argc, char *argv[], tlv_request_t *user_request) {
     // Checking the number of args
-    if (argc != 6)
-    {
-        printf(
-            "Usage: ./user <count_id> \"<password>\" <delay_mils> "
-            "<type_operation> \"<arguments_of_operations>\"\n");
+    if (argc != 6) {
+        printf( "Usage: ./user <count_id> \"<password>\" <delay_mils> <type_operation> \"<arguments_of_operations>\"\n");
         return 1;
     }
 
     // Preparing main arguments request
-    if (prepareMainArgs(argv, user_request))
-        return RC_OTHER;
+    if (prepareMainArgs(argv, user_request)) return RC_OTHER;
 
     // Preparing type of operation arguments request
-    if (prepareTypeOfOpArgs(argv, user_request))
-    {
+    if (prepareTypeOfOpArgs(argv, user_request)) {
         printf("Error in type of Op Arguments!\n");
         return RC_OTHER;
     }
 
     // Writing Log
     int fd = open(USER_LOGFILE, O_WRONLY | O_APPEND | O_CREAT, 0777);
-    if (logRequest(fd, (int)getpid(), user_request) < 0)
-    {
+    if (logRequest(fd, (int)getpid(), user_request) < 0) {
         printf("Failed to open and write into %s\n", USER_LOGFILE);
         return RC_OTHER;
     }
-    
+
     return RC_OK;
 }
 
-int createAccountUser(req_create_account_t *create, char argv[])
-{
+int createAccountUser(req_create_account_t *create, char argv[]) {
     char *token;
     int n = 0;
 
@@ -122,13 +108,10 @@ int createAccountUser(req_create_account_t *create, char argv[])
     token = strtok(argv, " ");
 
     /* walk through other tokens */
-    while (token != NULL)
-    {
-        if (n == 0)
-        {
+    while (token != NULL) {
+        if (n == 0) {
             create->account_id = strtoul(token, NULL, 10);
-        }
-        else if (n == 1)
+        } else if (n == 1)
             create->balance = strtoul(token, NULL, 10);
         else if (n == 2)
             strcpy(create->password, token);
@@ -139,8 +122,7 @@ int createAccountUser(req_create_account_t *create, char argv[])
         n++;
     }
 
-    if (n != 3)
-    {
+    if (n != 3) {
         printf("Error in create Account arguments\n");
         return -1;
     }
@@ -149,8 +131,7 @@ int createAccountUser(req_create_account_t *create, char argv[])
 }
 
 int transferOperation(uint32_t idOrigin, req_transfer_t *transfer,
-                      char argv[])
-{
+                      char argv[]) {
     char *token;
     int n = 0;
 
@@ -158,19 +139,15 @@ int transferOperation(uint32_t idOrigin, req_transfer_t *transfer,
     token = strtok(argv, " ");
 
     /* walk through other tokens */
-    while (token != NULL)
-    {
-        if (n == 0)
-        {
-            if (idOrigin == strtoul(token, NULL, 10))
-            {
+    while (token != NULL) {
+        if (n == 0) {
+            if (idOrigin == strtoul(token, NULL, 10)) {
                 printf("Same Source and Destination account.\n");
                 return RC_SAME_ID;
             }
 
             transfer->account_id = strtoul(token, NULL, 10);
-        }
-        else if (n == 1)
+        } else if (n == 1)
             transfer->amount = strtoul(token, NULL, 10);
         else
             break;
@@ -179,8 +156,7 @@ int transferOperation(uint32_t idOrigin, req_transfer_t *transfer,
         n++;
     }
 
-    if (n != 2)
-    {
+    if (n != 2) {
         printf("Error in transfer Operation arguments\n");
         return RC_OTHER;
     }
